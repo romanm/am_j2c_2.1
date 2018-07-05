35c17c16663bf289dd4ba035dbef7c382cd5a8b6
romanm <roman.mishchenko@gmail.com>
2018-07-06 01:58:23 +0300

    EMR second commit

---
+++ src/main/resources/static/f/eh2/abk1/abk.js
@@ -0,0 +1,72 @@
+init_am_directive.init_abk = function($scope, $http, $filter, $route) {
+	init_am_directive.ehealth_declaration($scope, $http, $filter);
+	console.log($scope.progr_am.viewes.objKeys())
+	console.log($scope.progr_am.viewes)
+	$scope.progr_am.viewes={
+		json_form:{
+			ngInclude:'/f/eh2/abk1/abk1.html',
+			dataName:'abk',
+			heightProcent:65,
+		},
+	}
+	addViews_abk_MenuJ2c()
+	$scope.include.add_j2c_menu = '/f/eh2/abk1/add_j2c_menu.html'
+	
+	$scope.progr_am.abk = {}
+	init_f74_ngClick($scope.progr_am.abk, $scope, $http)
+	console.log($scope.progr_am.abk)
+		
+	$scope.work_abk_record = function(){
+		console.log($scope.icpc2_nakaz74.selectedCell)
+		console.log('-----17-------')
+	}
+	
+	$scope.read_allpatient_records = function(){
+		var params = {
+			sql:sql3.f74_read_allpatient_records(),
+			table_id:9774,
+			msp_id:188,
+		}
+		exe_fn.httpGet(exe_fn.httpGet_j2c_table_params_then_fn(
+		params,
+		function(response) {
+			$scope.icpc2_nakaz74.data.list
+				= response.data.list
+		}))
+		$scope.isPatientSelected = false;
+	}
+
+	$scope.$watch('progr_am.icpc2_nakaz74.selectedCell',function(newValue){if(newValue){
+		console.log('-------- read patient racords --------------')
+		console.log($scope.progr_am.icpc2_nakaz74.selectedCell)
+		$scope.read_patient_records()
+	}
+	})
+	
+	$scope.isPatientSelected = false;
+	
+	$scope.read_patient_records = function(){
+		$scope.isPatientSelected = true;
+		console.log($scope.icpc2_nakaz74.selectedCell)
+		var row_patient_cell_id = $scope.icpc2_nakaz74.selectedCell.row.col_10766_id
+		console.log(row_patient_cell_id)
+		var params = {
+			sql:sql3.f74_read_patient_records(),
+			table_id:9774,
+			msp_id:188,
+			row_patient_cell_id:row_patient_cell_id,
+		}
+		exe_fn.httpGet(exe_fn.httpGet_j2c_table_params_then_fn(
+		params,
+		function(response) {
+			$scope.icpc2_nakaz74.data.list
+				= response.data.list
+			/*
+				console.log($scope.icpc2_nakaz74)
+				console.log($scope.icpc2_nakaz74.data.list)
+				console.log(response.data)
+				console.log(response.data.list)
+			 * */
+		}))
+	}
+}
---
+++ src/main/resources/static/f/eh2/abk1/read_abk.js
@@ -0,0 +1,98 @@
+console.log('---read_abk.js---')
+init_am_directive.ehealth_declaration_pageGroup = function($scope, $http, $filter){
+	console.log('---read_abk.js---ehealth_declaration_pageGroup---')
+	$scope.include = {
+		j2c_table_content:'/f/eh2/calendar/calendar_j2c_table_content.html',
+		icpc2_cell_dropdown_content:
+			'/f/eh2/icpc2_test4/icpc2_cell_dropdown_content_o74.html',
+	}
+
+	addViews_abk_MenuJ2c = function(){
+		$scope.progr_am.viewes.menu = {
+			ngInclude:'/f/eh1/info2/hrm_cards/hrm_menu.html',
+			seek:null,
+		}
+		$scope.progr_am.viewes.j2c_table = {
+			ngInclude:'/f/eh2/j2c_table.html',
+			dataName:'icpc2_nakaz74',
+			heightProcent:22,
+		}
+	}
+	
+	console.log('-------------------5--------------------')
+	$scope.progr_am.icpc2_nakaz74={}
+
+	$scope.progr_am.icpc2_nakaz74.httpGet = {
+		url:'/r/read2_sql_with_param',
+		params:{
+			sql:'sql2.j2c_table.selectByIdDesc',msp_id:188,table_id:9774
+		},
+		then_fn:function(response) {
+			delete response.data.add_joins
+			delete response.data.add_columns
+			delete response.data['sql2.j2c_table.selectByIdDesc']
+			/*
+				console.log($scope.icpc2_nakaz74)
+				console.log($scope.progr_am.icpc2_nakaz74)
+			 * */
+			$scope.icpc2_nakaz74.data = response.data
+			$scope.icpc2_nakaz74.mapDate = {}
+			angular.forEach($scope.icpc2_nakaz74.data.list,function(v){
+				var mapDateKey = $filter('date')(v.created, 'shortDate'),
+				h = $filter('date')(v.created, 'H')
+				if(!$scope.icpc2_nakaz74.mapDate[mapDateKey])
+					$scope.icpc2_nakaz74.mapDate[mapDateKey]= {list:[],hours:{}}
+				var mapDateValue = $scope.icpc2_nakaz74.mapDate[mapDateKey]
+				mapDateValue.list.push(v)
+				if(!mapDateValue.hours[h])
+					mapDateValue.hours[h] = []
+				mapDateValue.hours[h].push(v)
+			})
+			console.log($scope.icpc2_nakaz74.mapDate)
+			$scope.progr_am.icpc2_nakaz74.data
+			= $scope.icpc2_nakaz74.data
+			console.log('-------------------75--------------------')
+//			init_ngClick($scope.progr_am.icpc2_nakaz74)
+			init_f74_ngClick($scope.progr_am.icpc2_nakaz74, $scope, $http);
+		}
+	}
+
+	
+	init0_f74_ngClick = function(icpc2_nakaz74, $scope, $http){
+		icpc2_nakaz74.isEditRow = function(row){
+			return icpc2_nakaz74.selectedCell &&
+			icpc2_nakaz74.selectedCell.row_id==row.row_id
+//			return $scope.editRow && row.row_id == $scope.editRow.row_id
+		}
+	}
+
+	init0_f74_ngClick($scope.progr_am.icpc2_nakaz74, $scope, $http)
+
+	$scope.progr_am.icpc2_nakaz74.init_data = {
+		col_values:{
+			col_9776:{
+				1:'амбулаторно',
+				2:'вдома',
+				3:'по телефону',
+			},
+			col_9775:{
+				1:'первинне',
+				2:'повторне',
+				3:'зевершення епізоду',
+			},
+			col_10900:{
+				1:'місто',
+				2:'село',
+			},
+		},
+		include_cols:'/f/eh2/icpc2_test4/icpc2_cell_content_o74.html',
+		i1nclude_cols:'/f/eh1/info1/test3/icpc2_test3_cols.html',
+		col_sort:['creat_date', 'col_10766', 'col_9775', 'col_10771', 'col_10777', 'col_10807'
+			,'col_9776', 'col_10900'],
+	}
+	$scope.progr_am.icpc2_nakaz74.init_data.include_table_menu 
+		= '/f/eh2/table_menu.html'
+
+	console.log(	$scope.progr_am.icpc2_nakaz74)
+	
+}
