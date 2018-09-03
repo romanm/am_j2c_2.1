f01adacac91f7944887d596bd617cae145df4ec7
roman <roman@algoritmed2>
2018-09-04 00:54:05 +0300

    dialise_1 v.02

---
+++ src/main/resources/static/f/am-dev/dialise_1/dialise_1.js
@@ -0,0 +1,24 @@
+init_am_directive.init_dialise_1 = function($scope, $http, $filter, $route) {
+	console.log(123)
+	$scope.dialise_card = {
+		row_keys:{
+			r_1:'швидкість',
+			r_2:'АТ',
+			r_3:'PS',
+			r_4:'Введеня',
+		},
+		col_keys:{
+			c_1:'Показник',
+			с_2:'ч. 0,5',
+			с_3:'1',
+			с_4:'1,5',
+			с_5:'2',
+			с_6:'2,5',
+			с_7:'3',
+			с_8:'3,5',
+			с_9:'4',
+			с_10:'4,5',
+			с_11:'5',
+		},
+	}
+}
