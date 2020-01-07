0712c2381f17a43a4f52a3465db26c978a46215c
romanm <roman.mishchenko@gmail.com>
2020-01-07 22:01:45 +0200

    x

--- src/main/java/com/algoritmed/am_j2c_2/components/Filter02LoginRedirectPageKey.java
+++ src/main/java/com/algoritmed/am_j2c_2/components/Filter02LoginRedirectPageKey.java
@@ -11,16 +11,23 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
+import com.algoritmed.am_j2c_2.EhDb1Rest;
 
 @Component
 public class Filter02LoginRedirectPageKey implements Filter{
+	protected static final Logger logger = LoggerFactory.getLogger(Filter02LoginRedirectPageKey.class);
+
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
-        HttpServletRequest request2 = (HttpServletRequest)request;
+		logger.info("------27-------\n");
+		HttpServletRequest request2 = (HttpServletRequest)request;
 		String url = request2.getRequestURL().toString();
-        String queryString = request2.getQueryString();
+		String queryString = request2.getQueryString();
 		if(url.contains("login") && queryString!=null) {
 			String pageKey = (String) request2.getParameter("pageKey");
 			if(pageKey!=null) {
